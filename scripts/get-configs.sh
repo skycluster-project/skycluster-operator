6d579d5839ae2cbabf35d1a39e602e93034a301a
Ehsan E. <eh.etesami@gmail.com>
2025-10-30 21:07:04 +0000

    Update config fetch script to discard unavailable clusters

--- scripts/get-configs.sh
+++ scripts/get-configs.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 # Change these values to match your clusters
-AWS=xk-aws-us-east--78uib
-GCP=xk-gcp-us-east1-rhdru-q4rdv
+AWS=xk-aws-us-east--4z74l
+GCP=xk-gcp-us-east1-thams-zhsn7
 GCP_LOC=us-east1-b
 
 skycluster xkube config -k $AWS > /tmp/aws1
-AWS_CONTEXT=$(KUBECONFIG=/tmp/aws1 kubectl config get-contexts -o name)
-KUBECONFIG=/tmp/aws1 kubectl config rename-context $AWS_CONTEXT aws
+if [ $? -eq 0 ]; then
+  AWS_CONTEXT=$(KUBECONFIG=/tmp/aws1 kubectl config get-contexts -o name)
+  echo "AWS Context: $AWS_CONTEXT"
+  KUBECONFIG=/tmp/aws1 kubectl config rename-context $AWS_CONTEXT aws
+  echo "Renamed AWS context to 'aws'"
+else
+  echo "Failed to get AWS kubeconfig. Please ensure skycluster is installed and configured."
+fi
 
+echo "Fetching GCP cluster credentials..."
+KUBECONFIG=/tmp/gcp1 kubectl config delete-context gke || true
 KUBECONFIG=/tmp/gcp1 gcloud container clusters get-credentials "$GCP" --location "$GCP_LOC"
-GCP_CONTEXT=$(KUBECONFIG=/tmp/gcp1 kubectl config get-contexts -o name)
-KUBECONFIG=/tmp/gcp1 kubectl config rename-context $GCP_CONTEXT gke
+if [ $? -eq 0 ]; then
+  echo "Fetched GCP cluster credentials. Renaming context..."
+  GCP_CONTEXT=$(KUBECONFIG=/tmp/gcp1 kubectl config get-contexts -o name)
+  echo "GCP Context: $GCP_CONTEXT"
+  KUBECONFIG=/tmp/gcp1 kubectl config rename-context $GCP_CONTEXT gke
+  echo "Renamed GCP context to 'gke'"
+else
+  echo "Failed to get GCP cluster credentials. Please ensure gcloud is installed and configured."
+fi
 
 export KUBECONFIG=~/.kube/config:/tmp/gcp1:/tmp/aws1
 
